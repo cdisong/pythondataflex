f32c857f77ff17645698180ef6394420c1dd355f
Christine Song <christinedisong@gmail.com>
2018-01-10 12:56:52 -0800

    delete unsused code

--- datasetclean.py
+++ datasetclean.py
@@ -14,12 +14,4 @@ from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
 from scipy.optimize import curve_fit
-plt.rcParams["patch.force_edgecolor"] = True
-plt.style.use('fivethirtyeight')
-mpl.rc('patch', edgecolor = 'dimgray', linewidth=1)
-from IPython.core.interactiveshell import InteractiveShell
-InteractiveShell.ast_node_interactivity = "last_expr"
-pd.options.display.max_columns = 50
-%matplotlib inline
-warnings.filterwarnings("ignore")
 
